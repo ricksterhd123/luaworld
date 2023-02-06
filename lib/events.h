57d08100d91e73c7fa8063d54884d5b4dfaf47d3
ricksterhd123 <ricksterhd123@gmail.com>
2023-02-06 01:59:05 +0000

    argument validation addEventHandler

--- lib/events.h
+++ lib/events.h
@@ -30,7 +30,36 @@ void removeEventHandler(const char *eventName, int handlerFnRef)
 
 static int lua_addEventHandler(lua_State *L)
 {
-    const char* eventName = lua_tostring(L, 1);
+    char errorMsg[80];
+    int argc = lua_gettop(L);
+    if (argc <= 1)
+    {
+        sprintf(errorMsg, "'addEventHandler' expected 2 arguments, only got %d", argc);
+        lua_pushstring(L, errorMsg);
+        lua_error(L);
+        return 0;
+    }
+
+    int arg1 = lua_type(L, 1);
+    int arg2 = lua_type(L, 2);
+
+    if (lua_type(L, 1) != LUA_TSTRING)
+    {
+        sprintf(errorMsg, "'addEventHandler' expected string at argument 1, got %s instead", lua_typename(L, arg1));
+        lua_pushstring(L, errorMsg);
+        lua_error(L);
+        return 0;
+    }
+
+    if (lua_type(L, 2) != LUA_TFUNCTION)
+    {
+        sprintf(errorMsg, "'addEventHandler' expected function at argument 2, got %s instead", lua_typename(L, arg2));
+        lua_pushstring(L, errorMsg);
+        lua_error(L);
+        return 0;
+    }
+
+    const char *eventName = lua_tostring(L, 1);
     int handlerFnRef = luaL_ref(L, LUA_REGISTRYINDEX);
     addEventHandler(eventName, handlerFnRef);
     return 0;
